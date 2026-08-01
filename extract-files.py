fa06f9d7af95e3cbb4e4b9274133326a7ac46b27
WilliamSado <williamsado1004@gmail.com>
2026-08-01 05:46:39 +0000

    alioth: Fixup namespace

--- extract-files.py
+++ extract-files.py
@@ -64,9 +64,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 namespace_imports = [
-    'hardware/qcom-caf/common/libqti-perfd-client',
-    'hardware/qcom-caf/sm8250',
-    'vendor/qcom/opensource/display',
+    'vendor/qcom/common/vendor/adreno/r',
     'vendor/xiaomi/sm8250-common',
 ]
 
